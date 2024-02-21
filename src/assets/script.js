c1b9aa707d7af58e593f6b03220e55d94b23e5fd
Alícia Antunes <antunesalicia07@gmail.com>
2024-02-21 10:24:21 +0000

    feat: create increase/decrease functions

--- src/assets/script.js
+++ src/assets/script.js
@@ -58,24 +58,39 @@ function addProductToCart (productId) {
       cart.push({ ...product, quantity: 1 });
     }
   }
-}
+} 
 
 /* Create a function named increaseQuantity that takes in the productId as an argument
   - increaseQuantity should get the correct product based on the productId
   - increaseQuantity should then increase the product's quantity
 */
+function increaseQuantity(productId) {
+  let cartProduct = cart.find((item) => item.productId === productId);
+  if (cartProduct) {
+    cartProduct.quantity++;
+  }
+}
 
 /* Create a function named decreaseQuantity that takes in the productId as an argument
   - decreaseQuantity should get the correct product based on the productId
   - decreaseQuantity should decrease the quantity of the product
   - if the function decreases the quantity to 0, the product is removed from the cart
 */
+function decreaseQuantity (productId) {
+  let cartProduct = cart.find((item) => item.productId === productId);
+  if (cartProduct) {
+    cartProduct.quantity--;
+  }
+}
 
 /* Create a function named removeProductFromCart that takes in the productId as an argument
   - removeProductFromCart should get the correct product based on the productId
   - removeProductFromCart should update the product quantity to 0
   - removeProductFromCart should remove the product from the cart
 */
+function removeProductFromCart (productId) {
+  cart = cart.filter((item) => item.productId !== productId); 
+}
 
 /* Create a function named cartTotal that has no parameters
   - cartTotal should iterate through the cart to get the total of all products
